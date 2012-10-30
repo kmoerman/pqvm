f86d40d1b9eb6a08f3c55c7f3b5ebbc968e3360b
Klaas Moerman <kmoerman@vub.ac.be>
2012-10-30 19:53:35 +0100

    Initial Commit

---
+++ pqvm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  pqvm
+//
+//  Created by Klaas Moerman on 30/10/12.
+//  Copyright (c) 2012 vub. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
