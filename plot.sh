f0a6ab151566afe4871e461e76aae8417a734d5a
kmoerman <kmoerman@vub>
2012-12-07 21:02:32 +0100

    new plots

--- plot.sh
+++ plot.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 gnuplot <<- EOF
-	set xlabel "Threads"
-	set ylabel "Execution time [s]"
+	#set xlabel "Threads"
+	#set ylabel "Execution time [s]"
 	set term png
 	set output "plot/$1.png"
-	plot "build/$1.dat" using 1:2 title "$1"
+	set style data boxplot
+	plot "build/$1.dat" using (1.0):2:(0):1 title "$1"
 EOF
