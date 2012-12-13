7f9365ea23e1161c0c605bdb3721c99acf318372
kmoerman <kmoerman@vub>
2012-12-13 21:26:25 +0100

    improve log

--- measure/measure.cpp
+++ measure/measure.cpp
@@ -34,25 +34,25 @@ void measure (std::string& name, size_t iterations, void (*S)(), void (*P)()) {
     time_data << "# Execution time for " << iterations << " iterations on " << threads.first << " - " << threads.last << " threads." << std::endl;
     
     //Measure sequential average time as speedup reference
-    log << "Sequential algorithm." << std::endl << "Iterations: ";
+    log << "# SEQUENTIAL" << std::endl << "Iterations: " << iterations << std::endl;
     double s_time = 0;
     tbb_time_t start;
     for (int i = 0; i < iterations; ++i) {
         start = time();
         S();
         s_time += diff(start, time());
-        log << i + 1<< " ";
     }
     speedup_data << "# Average sequential time: " << s_time/iterations << " s." << std::endl;
+    log << "Sequential time: " << s_time/iterations << std::endl;
     
     //Measure parallel execution times
-    log << std::endl << std::endl << "Parallel algorithm." << std::endl;
+    log << std::endl << "# PARALLEL" << std::endl;
     time_data << "# threads" << separator << separator << "execution time [s]" << std::endl;
     speedup_data << "# threads" << separator << separator << "average speedup" << separator << "average execution time [s]"<<std::endl;
     double p_time = 0;
     double t_time = 0;
     for (int thread_n = threads.first; thread_n <= threads.last; ++thread_n) {
-        log << std::endl << "Threads: " << thread_n << std::endl << "Iterations: ";
+        log << "Threads: " << thread_n << std::endl << "Iterations: " << iterations << std::endl;
         tbb::task_scheduler_init init(thread_n);
 
         for (int i = 0; i < iterations; ++i) {
@@ -61,10 +61,9 @@ void measure (std::string& name, size_t iterations, void (*S)(), void (*P)()) {
             p_time = diff(start, time());
             t_time += p_time;
             time_data << thread_n << separator << p_time << separator << std::endl;
-            log << i + 1 << " ";
         }
         speedup_data << thread_n << separator << s_time/t_time << separator << t_time/iterations << std::endl;
-        log << std::endl << "Speedup: " << s_time/t_time << std::endl;
+        log << "Speedup: " << s_time/t_time << std::endl;
     }
     
     speedup_data.close();
